5978fa615d0a7ac6c3528cbef0df6a5429b05f0e
GaneshThakur <tagorganesh464@gmail.com>
2023-06-21 11:23:12 +0530

    Final Task

--- frontend/src/components/empProfile/EmpProfile.js
+++ frontend/src/components/empProfile/EmpProfile.js
@@ -4,11 +4,10 @@ import { taskContext } from "../../context/TasksContextProvider";
 import { useForm } from "react-hook-form";
 import { loginContext } from "../../context/loginContext";
 
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "./empProfile.css";
-import Tilt from "react-vanilla-tilt"
-
+import Tilt from "react-vanilla-tilt";
 
 const EmpProfile = () => {
   let [tasks, setTasks] = useContext(taskContext);
@@ -21,7 +20,6 @@ const EmpProfile = () => {
     loginUser,
     logoutUser,
     role,
-    
   ] = useContext(loginContext);
   let {
     register,
@@ -72,14 +70,14 @@ const EmpProfile = () => {
     setValue("email", userObj?.email);
     setValue("phone", userObj?.phone);
   };
-  //   saveModifiedUser
+
+  // save modified user
   const saveModifiedUser = () => {
-    
-    if(Object.keys(errors).length===0){
-      let modifieduser = getValues();
+    if (Object.keys(errors).length === 0) {
+      let modifiedUser = getValues();
 
       axios
-        .put(`http://localhost:5000/user-api/update-user`, modifieduser, {
+        .put(`http://localhost:5000/user-api/update-user`, modifiedUser, {
           headers: { Authorization: "Bearer " + token },
         })
         .then((response) => {
@@ -97,11 +95,9 @@ const EmpProfile = () => {
             setError(err.message);
           }
         });
-  
-        handleClose();
+
+      handleClose();
     }
-    
-   
   };
 
   useEffect(() => {
@@ -110,26 +106,22 @@ const EmpProfile = () => {
 
   return (
     <div className="container users-data d-flex justify-content-center align-items-center emp-profil">
-       <Tilt className="bg-transparent p-0 text-white border-0 rounded-0 lh-0 shadow-none d-bolck m-auto mt-5" style={{width:"800px"}}>
-      <div className="emp-style">
-     
-          <div class="employee">
-  <h1 class="name">{tasks?.username}</h1>
-  <p class="department">Dept - {tasks?.department}</p>
-  <p class="email">Email - {tasks?.email}</p>
-  <p class="phone">Ph:  {tasks?.phone}</p>
-  <p class="doj">DOJ - {tasks?.jod}</p>
-  <button className="btn btn-warning float-start" onClick={() => editUser(tasks)}>
-  Edit
-</button>
-
-</div>
-
-     
-      </div>
+      <Tilt className="bg-transparent p-0 text-white border-0 rounded-0 lh-0 shadow-none d-bolck m-auto mt-5" style={{ width: "800px" }}>
+        <div className="emp-style">
+          <div className="employee">
+            <h1 className="name">{tasks?.username}</h1>
+            <p className="department">Dept - {tasks?.department}</p>
+            <p className="email">Email - {tasks?.email}</p>
+            <p className="phone">Ph: {tasks?.phone}</p>
+            <p className="doj">DOJ - {tasks?.jod}</p>
+            <button className="btn btn-warning float-start" onClick={() => editUser(tasks)}>
+              Edit
+            </button>
+          </div>
+        </div>
       </Tilt>
 
-      {/* modal to edit user */}
+      {/* Modal to edit user */}
       <Modal
         show={show}
         onHide={handleClose}
@@ -139,79 +131,99 @@ const EmpProfile = () => {
       >
         <Modal.Body className="edit-form block">
           <h3 className="fw-bold mb-4 pb-2 pb-md-0 mb-md-5 text-center">Edit Profile</h3>
-          {/* edit form */}
+          {/* Edit form */}
           <form onSubmit={handleSubmit(saveModifiedUser)}>
-            {/* name */}
-            <div className="inputbox form-floating mb-3">
-            <input
-                    type="text"
-                    id="username"
-                    className="form-control "
-                    placeholder="xyz"
-                    {...register("username", {
-                      required: true,
-                      minLength: 4,
-                      maxLength: 10,
-                    })}
-                  ></input>
-                  <label htmlFor="username" className="text-dark">
-                    User Name
-                  </label>
-
-                  {errors.username?.type === "required" && (
-                    <p className=" text-danger">*enter your first name</p>
-                  )}
-                  {errors.username?.type === "minLength" && (
-                    <p className=" text-danger">
-                      *minimum 4 letter word is required
-                    </p>
-                  )}
-                  {errors.username?.type === "maxLength" && (
-                    <p className=" text-danger">
-                      *maximum 6 letter word is required
-                    </p>
-                  )}
-            </div>
+            {/* Username */}
             <div className="inputbox form-floating mb-3">
               <input
                 type="text"
-                className="form-control form-inp text-dark"
-                id="password"
+                id="username"
+                className="form-control form-inp"
+                {...register("username", {
+                  required: true,
+                  minLength: 4,
+                  maxLength: 10,
+                })}
                 placeholder="xyz"
-                {...register('password')}
               />
-              <label htmlFor="password">Password</label>
+              <label htmlFor="username" className="text-dark">
+                User Name
+              </label>
+
+              {errors.username?.type === "required" && (
+                <p className="text-danger">* Enter your username</p>
+              )}
+              {errors.username?.type === "minLength" && (
+                <p className="text-danger">
+                  * Minimum 4 characters for the username are required
+                </p>
+              )}
+              {errors.username?.type === "maxLength" && (
+                <p className="text-danger">
+                  * Maximum 10 characters for the username are allowed
+                </p>
+              )}
             </div>
-              {/* email */}
-              <div className="inputbox form-floating mb-3">
+
+            {/* Password */}
+            <div className="inputbox form-floating mb-3">
+              <input
+                type="text"
+                id="password"
+                className="form-control form-inp"
+                placeholder="xyz"
+                {...register("password", {
+                  required: true,
+                  minLength: 4,
+                })}
+              />
+              <label htmlFor="password" className="text-dark">
+                Password
+              </label>
+
+              {errors.password?.type === "required" && (
+                <p className="text-danger">* Enter your password</p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-danger">
+                  * Minimum 4 characters for the password are required
+                </p>
+              )}
+            </div>
+
+            {/* Email */}
+            <div className="inputbox form-floating mb-3">
               <input
                 type="email"
                 className="form-control form-inp"
                 id="email"
                 placeholder="email"
-                {...register('email')}disabled
+                {...register("email")}
+                disabled
               />
               <label htmlFor="email">Email</label>
             </div>
-            {/* department */}
+
+            {/* Department */}
             <div className="inputbox form-floating mb-3">
               <input
                 type="text"
                 className="form-control form-inp"
                 id="department"
                 placeholder="Department"
-                {...register('department')}
+                {...register("department")}
               />
               <label htmlFor="department">Department</label>
             </div>
-            {/* phone */}
+
+            {/* Phone Number */}
             <div className="inputbox form-floating mb-3">
               <input
                 type="tel"
                 className="form-control form-inp"
                 id="phone"
                 placeholder="Phone Number"
-                {...register('phone')}
+                {...register("phone")}
               />
               <label htmlFor="phone">Phone Number</label>
             </div>
@@ -221,7 +233,7 @@ const EmpProfile = () => {
               <Button variant="danger" className="btn-sm me-2" onClick={handleClose}>
                 Close
               </Button>
-              <Button variant="primary" className="btn-sm" onClick={saveModifiedUser}>
+              <Button variant="primary" className="btn-sm" type="submit">
                 Save Changes
               </Button>
             </div>
@@ -230,6 +242,6 @@ const EmpProfile = () => {
       </Modal>
     </div>
   );
-}
+};
 
 export default EmpProfile;
